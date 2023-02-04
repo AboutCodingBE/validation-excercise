7879e4289e697d78ee3a0be0244fb707f2d1c68f
mahdignb <mahdignb@hotmail.com>
2023-02-04 21:01:11 +0330

    Changed project type from Console to WebApi

--- SensorValidation/SensorValidation/Program.cs
+++ SensorValidation/SensorValidation/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace SensorValidation
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+var builder = WebApplication.CreateBuilder(args);
 
-        }
-    }
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseStaticFiles();
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
