350baef55990b57bf16179ebb9b6551f67746db9
mahdignb <mahdignb@hotmail.com>
2023-02-04 21:02:28 +0330

    Added new enums SensorStatus and TaskStatus enums

---
+++ SensorValidation/SensorValidation/Models/Enums/SensorStatusEnums.cs
@@ -0,0 +1,16 @@
+﻿namespace SensorValidation.Models
+{
+    public enum SensorStatus
+    {
+        Idle = 1,
+        Awaiting = 2,
+        Updating = 3,
+    }
+    public enum SensorActivityStatus
+    {
+        Online = 1,
+        Disconnected = 2,
+        Error = 3,
+    }
+
+}
---
+++ SensorValidation/SensorValidation/Models/Enums/TaskEnums.cs
@@ -0,0 +1,15 @@
+﻿namespace SensorValidation.Models
+{
+    public enum TaskJobs
+    {
+        update_firmware = 0,
+        update_configuration = 1
+    }
+    public enum TaskStatus
+    {
+        Pending = 0,
+        Completed = 1,
+        Error = 2,
+        Ongoing = 3
+    }
+}
