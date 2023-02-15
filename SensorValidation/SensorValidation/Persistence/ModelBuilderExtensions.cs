ac2532719087f26b938605330aeb526781f022b0
mahdignb <mahdignb@hotmail.com>
2023-02-15 08:36:33 +0330

    Database : Added seed data

---
+++ SensorValidation/SensorValidation/Persistence/ModelBuilderExtensions.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SensorValidation.Models;
+using SensorValidation.Models.Entities;
+using TaskStatus = SensorValidation.Models.TaskStatus;
+
+namespace SensorValidation.Persistence
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void SeedSensors(this ModelBuilder builder)
+        {
+            builder.Entity<SensorModel>().HasData(new SensorModel
+            {
+                Serial = 1,
+                CreatedAt = DateTime.UtcNow.AddDays(-20),
+                UpdatedAt = DateTime.UtcNow.AddDays(-20),
+                ActivityStatus = SensorActivityStatus.Error.ToString(),
+                CurrentConfiguration = "a.config",
+                CurrentFirmware = "FW:23-015-2023:03",
+                StatusName = SensorStatus.Idle.ToString(),
+                NextTask = 1,
+                TaskCount = 3,
+                StatusId = 1,
+                Type = "TS50X",
+            });
+            builder.Entity<SensorModel>().HasData(new SensorModel
+            {
+                Serial = 2,
+                CreatedAt = DateTime.UtcNow.AddDays(-20),
+                UpdatedAt = DateTime.UtcNow.AddDays(-20),
+                ActivityStatus = SensorActivityStatus.Error.ToString(),
+                CurrentConfiguration = "b.config",
+                CurrentFirmware = "FW:23-015-2023:03",
+                StatusName = SensorStatus.Idle.ToString(),
+                NextTask = 2,
+                TaskCount = 3,
+                StatusId = 1,
+                Type = "TS50X"
+            });
+        }
+
+        public static void SeedTasks(this ModelBuilder builder)
+        {
+            builder.Entity<TaskModel>().HasData(new TaskModel
+            {
+                Id = 1,
+                CreatedAt = DateTime.UtcNow.AddDays(-19),
+                UpdatedAt = DateTime.UtcNow.AddDays(-19),
+                AttemptCount = 3,
+                FileId = "",
+                SensorSerial = 2,
+                StatusId = TaskStatus.Pending,
+                StatusName = TaskStatus.Pending.ToString(),
+                Type = TaskJobs.update_firmware.ToString(),
+            });
+            builder.Entity<TaskModel>().HasData(new TaskModel
+            {
+                Id = 2,
+                CreatedAt = DateTime.UtcNow.AddDays(-19),
+                UpdatedAt = DateTime.UtcNow.AddDays(-19),
+                AttemptCount = 3,
+                FileId = "",
+                SensorSerial = 1,
+                StatusId = TaskStatus.Pending,
+                StatusName = TaskStatus.Pending.ToString(),
+                Type = TaskJobs.update_firmware.ToString(),
+            });
+        }
+        public static void SeedTaskSensors(this ModelBuilder builder)
+        {
+            builder.Entity<TaskSensorModel>().HasData(new TaskSensorModel
+            {
+                Id = 1,
+                FileId = Guid.NewGuid().ToString(),
+                SensorSerial = 2,
+                Type = TaskJobs.update_configuration.ToString(),
+            });
+            builder.Entity<TaskSensorModel>().HasData(new TaskSensorModel
+            {
+                Id = 2,
+                FileId = Guid.NewGuid().ToString(),
+                SensorSerial = 1,
+                Type = TaskJobs.update_firmware.ToString(),
+            });
+        }
+    }
+}
---
+++ SensorValidation/SensorValidation/Persistence/SensorValidationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SensorValidation.Models.Entities;
+
+namespace SensorValidation.Persistence
+{
+    public class SensorValidationDbContext : DbContext
+    {
+        public SensorValidationDbContext(DbContextOptions<SensorValidationDbContext> option) : base(option)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.SeedSensors();
+            modelBuilder.SeedTasks();
+            modelBuilder.SeedTaskSensors();
+        }
+        public DbSet<SensorModel> Sensors { get; set; }
+        public DbSet<TaskModel> Tasks { get; set; }
+        public DbSet<TaskSensorModel> TaskSensors { get; set; }
+    }
+}
